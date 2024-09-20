a2c3ae2fdd21ee44b65f67b5dd87004e15ed9876
Chicken-is-my-Jam-8 <s1347556@monmouth.edu>
2024-09-20 12:40:38 -0400

    Update locker_combonation.py

--- locker_combonation.py
+++ locker_combonation.py
@@ -12,7 +12,6 @@ def lockerCombo():
 
     if num1 == num2 or num1 == num3 or num2 == num3:
         print()
-        print(f"{myName}'s locker combination is: {num1}-{num2}-{num3}")
         print("Please try again, this is not a valid combination")
         lockerCombo()
 
